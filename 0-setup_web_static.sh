ae10439d697034fa52dabf54fcbf062fda9ad9a3
Kimshubb <issakimanifmk@gmail.com>
2024-03-16 06:43:45 -0700

    permissions

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -15,7 +15,7 @@ echo "<html>
 </body>
 </html>" | sudo tee /data/web_static/releases/test/index.html > /dev/null
 sudo ln -sf /data/web_static/releases/test /data/web_static/current
-sudo chown -R www-data:www-data /data/
+sudo chown -R ubuntu:ubuntu /data/
 sudo sed -i '/server {/a \ \ \ \ location /hbnb_static/ { alias /data/web_static/current/; } }' /etc/nginx/sites-available/default
 
 sudo service nginx restart
