621490500ec23e9c7ea6da80321931b30f9a33df
Kimshubb <issakimanifmk@gmail.com>
2024-03-16 06:54:38 -0700

    fixed web static

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -4,19 +4,32 @@ if command -v nginx &> /dev/null; then
 	sudo apt-get update
 	sudo apt-get -y install nginx
 fi
-sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
-echo "<html>
-<head>
-    <title>Test Page</title>
-</head>
-<body>
-    <h1>This is a test page</h1>
-    <p>Hello, world!</p>
-</body>
-</html>" | sudo tee /data/web_static/releases/test/index.html > /dev/null
-sudo ln -sf /data/web_static/releases/test /data/web_static/current
-sudo chown -R ubuntu:ubuntu /data/
-sudo sed -i '/server {/a \ \ \ \ location /hbnb_static/ { alias /data/web_static/current/; } }' /etc/nginx/sites-available/default
+mkdir -p /data/web_static/releases/test/
+mkdir -p /data/web_static/shared/
+echo "Holberton School" > /data/web_static/releases/test/index.html
+ln -sf /data/web_static/releases/test/ /data/web_static/current
 
-sudo service nginx restart
-exit 0
+chown -R ubuntu /data/
+chgrp -R ubuntu /data/
+
+printf %s "server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    add_header X-Served-By $HOSTNAME;
+    root   /var/www/html;
+    index  index.html index.htm;
+    location /hbnb_static {
+        alias /data/web_static/current;
+        index index.html index.htm;
+    }
+    location /redirect_me {
+        return 301 http://cuberule.com/;
+    }
+    error_page 404 /404.html;
+    location /404 {
+      root /var/www/html;
+      internal;
+    }
+}" > /etc/nginx/sites-available/default
+
+service nginx restart
