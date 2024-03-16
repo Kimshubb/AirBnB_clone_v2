607bda8a3f0b0626fc7c4a0ffb3b269593e397ab
Kimshubb <issakimanifmk@gmail.com>
2024-03-16 05:32:59 -0700

    setip web static

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+#set up server for deployment of web static
+if command -v nginx &> /dev/null; then
+	sudo apt-get update
+	sudo apt-get -y install nginx
+fi
+sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
+echo "<html>
+<head>
+    <title>Test Page</title>
+</head>
+<body>
+    <h1>This is a test page</h1>
+    <p>Hello, world!</p>
+</body>
+</html>" | sudo tee /data/web_static/releases/test/index.html > /dev/null
+sudo ln -sf /data/web_static/releases/test /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data/
+sudo sed -i '/hbnb_static/!b;n;c\    alias /data/web_static/current/;' /etc/nginx/sites-available/default
+sudo service nginx restart
+exit 0
