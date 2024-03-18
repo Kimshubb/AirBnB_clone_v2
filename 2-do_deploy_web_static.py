b38b0764d2b07d80a70e679da9b096525ec24fc7
Kimshubb <issakimanifmk@gmail.com>
2024-03-18 09:13:10 -0700

    full deployment

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,48 +1,52 @@
 #!/usr/bin/python3
-"""Fabric script to deploy archive to my webservers"""
+"""
+Fabric script that distributes an archive to your web servers
+"""
+
 from datetime import datetime
 from fabric.api import *
 import os
 
-env.hosts = ["ubuntu@54.175.1.126", "ubuntu@3.83.253.4"]
+env.hosts = ["54.175.1.126", "3.83.253.4"]
+env.user = "ubuntu"
+
+
 def do_pack():
-    """Generate a tgz archive from web static folder contentds"""
-    try:
-        local("mkdir -p versions")
-        timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
-        archive = "versions/web_static_{}.tgz".format(timestamp)
-        print("Packing web static to {}".format(archive))
-        local("tar -cvzf {} web_static".format(archive))
-        size = os.stat(archive).st_size
-        print("Web static packed:{} -> {}Bytes".format(archive, size))
-        return "versions/{}".format(archive)
-    except Exception as e:
-        print("Error:", e)
+    """
+        return the archive path if archive has generated correctly.
+    """
+
+    local("mkdir -p versions")
+    date = datetime.now().strftime("%Y%m%d%H%M%S")
+    archived_f_path = "versions/web_static_{}.tgz".format(date)
+    t_gzip_archive = local("tar -cvzf {} web_static".format(archived_f_path))
+
+    if t_gzip_archive.succeeded:
+        return archived_f_path
+    else:
         return None
 
+
 def do_deploy(archive_path):
-    """Distribute archive to web servers and deploy"""
-    if not os.path.exists(archive_path):
-        print("Error: Archive {} does not exist".format(archive_path))
-        return False
-    try:
-        put(archive_path, '/tmp/')
-        archive_name = os.path.basename(archive_path)
-        release_folder = '/data/web_static/releases/' + os.path.splitext(archive_name)[0]
-        run('sudo mkdir -p {}'.format(release_folder))
-        run('sudo chmod 755 {}'.format(release_folder))
-        run('tar -xzf /tmp/{} -C {}'.format(archive_name, release_folder))
-        run('rm /tmp/{}'.format(archive_name))
-        run('rm -rf /data/web_static/current')
-        run('sudo ln -s {} /data/web_static/current'.format(release_folder))
+    """
+        Distribute archive.
+    """
+    if os.path.exists(archive_path):
+        archived_file = archive_path[9:]
+        newest_version = "/data/web_static/releases/" + archived_file[:-4]
+        archived_file = "/tmp/" + archived_file
+        put(archive_path, "/tmp/")
+        run("sudo mkdir -p {}".format(newest_version))
+        run("sudo tar -xzf {} -C {}/".format(archived_file,
+                                             newest_version))
+        run("sudo rm {}".format(archived_file))
+        run("sudo mv {}/web_static/* {}".format(newest_version,
+                                                newest_version))
+        run("sudo rm -rf {}/web_static".format(newest_version))
+        run("sudo rm -rf /data/web_static/current")
+        run("sudo ln -s {} /data/web_static/current".format(newest_version))
+
         print("New version deployed!")
         return True
-    except Exception as e:
-        print("{} Error during deployment!".format(e))
-if __name__ == "__main__":
-    archive_path = do_pack()
-    do_deploy(archive_path)
-    if archive_path:
-        do_deploy(archive_path)
-    else:
-        print("Archive creation failed. Deployment aborted!")
+
+    return False
