0dbc2cbc892a189d8a326850d990140e6a779023
sabimbi447 <asarejonas447@gmail.com>
2024-03-23 21:15:16 +0000

    Task 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,25 +1,27 @@
 #!/usr/bin/env bash
-# Installs Nginx if it not already installed
-# Creates a fake HTML file /data/web_static/releases/test/index.html
-# Creates a symbolic link /data/web_static/current linked to the /data/web_static/releases/test/ folder
-
-# install ngnix
-sudo apt-get -y update
+# Bash script that sets up web servers for the deployment of web_static
+sudo apt-get update
 sudo apt-get -y install nginx
 sudo ufw allow 'Nginx HTTP'
 
-# create index.html recursively
+sudo mkdir -p /data/
+sudo mkdir -p /data/web_static/
+sudo mkdir -p /data/web_static/releases/
 sudo mkdir -p /data/web_static/shared/
-sudo mkdir -p /data/web_static/releases/test/i
-echo "Hello World! From Shawn" > /data/web_static/releases/test/index.html
+sudo mkdir -p /data/web_static/releases/test/
+sudo touch /data/web_static/releases/test/index.html
+sudo echo "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" | sudo tee /data/web_static/releases/test/index.html
 
-# linking files
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo ln -s -f /data/web_static/releases/test/ /data/web_static/current
 
-# making ubuntu owner of files
-sudo chown -hR ubuntu:ubuntu /data
+sudo chown -R ubuntu:ubuntu /data/
 
-# Add server config
-sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
 
 sudo service nginx restart
