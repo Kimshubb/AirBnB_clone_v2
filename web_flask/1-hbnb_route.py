01fb6a7b2d0780bca4a19a4c8aae7228567e98bb
Kimshubb <issakimanifmk@gmail.com>
2024-04-16 13:54:12 +0000

    fix

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-"""this is neccesary"""
+"""start a flask web app"""
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
-def hello_hbnb():
+def index():
     """hello hbnb"""
     return 'Hello HBNB!'
 
